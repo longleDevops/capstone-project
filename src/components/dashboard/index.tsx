4447c49446ea38b4137fb58f55236d24214507cb
Jake16642 <jaketran1999@gmail.com>
2024-04-11 13:03:31 -0700

    edited dashboard page

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -4,6 +4,9 @@ import React, { useState } from 'react';
 import Button from "../ui/button"
 import TextInput from "../ui/inputField";
 import styles from "./style.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faQuestionCircle, faPowerOff, faCogs, faHandsHelping, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+
 
 
 export default function Dashboard() {
@@ -26,18 +29,20 @@ export default function Dashboard() {
             </div>
         </div>
         <div className={styles.div}>
-        <div className={styles.loginTitle}>Profile</div>
+            <div className={styles.loginTitle}>Profile</div>
             <div className={styles.loginContainer}>
-                <button className={styles.skipButton}>My Account</button>
-                <button className={styles.skipButton}>Take Survey</button>
-                <button className={styles.skipButton}>Log out</button>
+                <a href="/user-profile" className={styles.skipButton}><FontAwesomeIcon icon={faUser} /> My Account</a>
+                <a href="/take-survey" className={styles.skipButton}><FontAwesomeIcon icon={faQuestionCircle} /> Take Survey</a>
+                <a href="/logout" className={styles.skipButton}><FontAwesomeIcon icon={faPowerOff} /> Log out</a>
             </div>
-            <div className={styles.p}>More</div>
+            <div className={styles.p}>
+                More
+            </div>
             <div className={styles.loginContainer}>
-                <button className={styles.skipButton}>Help & Support</button>
-                <button className={styles.skipButton}>About App</button>
+                <a href="/help-support" className={styles.skipButton}><FontAwesomeIcon icon={faHandsHelping} /> Help & Support</a>
+                <a href="/about-app" className={styles.skipButton}><FontAwesomeIcon icon={faInfoCircle} /> About App</a>
             </div>
-            </div>
+        </div>
         </>
     );
 }

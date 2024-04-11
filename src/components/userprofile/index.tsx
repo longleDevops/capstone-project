b6cac51af8bba2255f2c82f0df8c91b04bb85a9f
Botir Babadzhanov <batyr.babadjanov@gmail.com>
2024-04-11 15:49:57 -0700

    edit user profile drop down menu css

--- src/components/userprofile/index.tsx
+++ src/components/userprofile/index.tsx
@@ -1,54 +1,57 @@
 "use client";
-import Link from "next/link";
+
 import React, { useState } from 'react';
-import Button from "../ui/button"
-import TextInput from "../ui/inputField";
 import styles from "./style.module.css";
 
+interface TextInputProps {
+    e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>;
+}
 
 export default function UserProfile() {
-    // States to store the input field values
-    const [phoneNumber, setPhoneNo] = useState('');
-    const [selectedGender, setSelectedGender] = useState('');
-    const [Major, setMajor] = useState('');
-    
-    const handleSelectChange = (e) => {
+    const [phoneNumber, setPhoneNo] = useState<string>('');
+    const [selectedGender, setSelectedGender] = useState<string>('');
+    const [Major, setMajor] = useState<string>('');
+
+    const handleSelectChange = (e: TextInputProps['e']) => {
         setSelectedGender(e.target.value);
     };
 
     return (
         <>
-        <div className={styles.container}>
-            <div className={styles.iconWrapper}>
-                {/* Display the icon image */}
-                <img src="/user-icon.jpg" alt="Icon" className={styles.icon} />
+            <div className={styles.container}>
+                <div className={styles.iconWrapper}>
+                    {/* Display the icon image */}
+                    <img src="/user-icon.jpg" alt="Icon" className={styles.icon} />
+                </div>
+                <div className={styles.loginContainer}>
+                    <div className={styles.loginTitle}>Bio</div>
+                    <div className={styles.inputWrapper}>
+                        <input
+                            className={styles.input}
+                            placeholder="Phone Number"
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNo(e.target.value)} />
+                    </div>
+                    <div>
+                        <select value={selectedGender} onChange={handleSelectChange} className={styles.dropDown}>
+                            <option value="" disabled selected className={styles.placeholderOption}>
+                                Select Gender
+                            </option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
+
+                    <div className={styles.inputWrapper}>
+                        <input
+                            className={styles.input}
+                            placeholder="What is your major?"
+                            value={Major}
+                            onChange={(e) => setMajor(e.target.value)} />
+                    </div>
+                    <button className={styles.skipButton}>Skip</button>
+                </div>
             </div>
-            <div className={styles.loginContainer}>
-                <div className={styles.loginTitle}>Bio</div>
-                <div className={styles.inputWrapper}>
-                    <input
-                        className={styles.input}
-                        placeholder="Phone Number"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNo(e.target.value)} />
-                </div>
-                <div className={styles.inputWrapper}>
-                    <select value={selectedGender} onChange={handleSelectChange} className={styles.input}>
-                        <option value="" disabled hidden>Select your gender</option>
-                        <option value="option1">Male</option>
-                        <option value="option2">Female</option>
-                    </select>
-                </div>
-                <div className={styles.inputWrapper}>
-                    <input
-                        className={styles.input}
-                        placeholder="What is your major?"
-                        value={Major}
-                        onChange={(e) => setMajor(e.target.value)} />
-                </div>
-                <button className={styles.skipButton}>Skip</button>
-            </div>
-        </div>
         </>
     );
 }

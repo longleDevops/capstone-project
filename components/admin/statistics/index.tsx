fe566dc8abffa0df78e66c49466b0c8208d75436
HUONG MAI <130521221+hmai97@users.noreply.github.com>
2024-05-07 09:33:55 -0700

    update statistics page

--- components/admin/statistics/index.tsx
+++ components/admin/statistics/index.tsx
@@ -1,20 +1,24 @@
 import Image from "next/image"
 import styles from "./styles.module.css"
+import { Pie } from "react-chartjs-2";
+import {PieChart} from "../statistics2/pie-chart"
+
 export const Statistics = () => {
+  const undergraduateData = 300;
+  const graduateData = 200;
+
   return (
+
     <div className={styles.container}>
-      <Image
-        src={'/prototype.png'}
-        alt="prototype"
-        width={1100}
-        height={700}
-      />
-      <Image
-        src={'/prototype2.png'}
-        alt="prototype"
-        width={1100}
-        height={700}
-      />
+      <div className={styles.title}>
+        Survey Statistics 
+      </div>
+      <div className={styles.studentStatus}>
+         <p>Student Status</p>
+          <PieChart/>
+      </div>
+
+      
     </div>
   )
 }
--- components/admin/statistics2/pie-chart.tsx
+++ components/admin/statistics2/pie-chart.tsx
@@ -1,9 +1,29 @@
+"use client"
 import styles from "./styles.module.css"
+import { Pie } from "react-chartjs-2"
+import 'chart.js/auto';
+
 
 export const PieChart = () => {
+  const data = {
+    labels: [
+      'Undergraduate',
+      'Graduated',
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [50, 30],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+      ],
+      hoverOffset: 4
+    }]
+  };
+
   return (
-    <div>
-      pie
+    <div className={styles.pie_container}>
+      <Pie data={data} />
     </div>
   )
 }

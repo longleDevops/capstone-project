5390077d4fbcfdc8405881a1f253b79ed9821687
Jake16642 <jaketran1999@gmail.com>
2024-05-15 15:47:03 -0700

    fix: add new survey question in background check

--- components/student/survey2/part-one/survey-p1.tsx
+++ components/student/survey2/part-one/survey-p1.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { Input, Select } from '@mantine/core';
+import { Input, Select, } from '@mantine/core';
+import { MonthPickerInput } from '@mantine/dates';
 import styles from './styles.module.css'
 import { useState } from 'react';
 import { BriefcaseBusiness, ScanFace } from 'lucide-react';
@@ -9,9 +10,16 @@ export const SurveyOne = () => {
 
   const [value, setValue] = useState('Long')
   const [major, setMajor] = useState<string | null>('');
+  const [start, setStartTerm] = useState<Date | null>(null);
+  const [end, setEndTerm] = useState<Date | null>(null);
+  const [campus, setCampus] = useState<string | null>('');
+  const [gender, setGender] = useState<string | null>('');
+  const [race, setRace] = useState<string | null>('');
+
 
   return (
     <div className={styles.container}>
+      <div className={styles.pagetitle}>Background Info</div>
       <div className={styles.title}>What is your first name?</div>
       <Input size='lg' radius={20} placeholder='your first name' required value={value} styles={{ input: {} }}
         onChange={(e) => setValue(e.target.value)} />
@@ -27,6 +35,7 @@ export const SurveyOne = () => {
       <Select
         size="lg"
         radius={10}
+        value={major}
         leftSection={<BriefcaseBusiness />}
         style={{}}
         placeholder="Example: Computer Science"
@@ -44,13 +53,76 @@ export const SurveyOne = () => {
           "Accounting",
           "Finance",
           "Nursing",
-          "Mathematic", // list of majors.
+          "Medical", // list of majors.
         ]}
         allowDeselect={false}
         searchable
         onChange={setMajor}
         nothingFoundMessage="Nothing found..."
         comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
+      />
+      <div className={styles.title}>When is your start term</div>
+      <MonthPickerInput
+        size='lg'
+        placeholder="Pick month"
+        value={start}
+        onChange={setStartTerm}
+      />
+      <div className={styles.title}>When is your end term</div>
+      <MonthPickerInput
+        size='lg'
+        placeholder="Pick month"
+        value={end}
+        onChange={setEndTerm}
+      />
+      <div className={styles.title}>Where is your campus location?</div>
+      <Select
+        size="lg"
+        radius={10}
+        value={campus}
+        style={{}}
+        placeholder="Example: Des Moines"
+        data={[
+          "Ellensburg",
+          "Pierce County",
+          "Lynnwood",
+          "Des Moines",
+          "Sammamish", // list of location.
+        ]}
+        allowDeselect={false}
+        searchable
+        onChange={setCampus}
+        nothingFoundMessage="Nothing found..."
+        comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
+      />
+      <div className={styles.title}>What is your gender?</div>
+      <Select
+        size="lg"
+        placeholder="Pick Gender"
+        data={[
+          'Male',
+          'Female',
+          'Other',
+        ]}
+        value={gender}
+        onChange={setGender}
+        clearable
+      />
+      <div className={styles.title}>What is your race?</div>
+      <Select
+        size="lg"
+        placeholder="Pick Race"
+        data={[
+          'Asian',
+          'White and European',
+          'African',
+          'Hispanic and Latino',
+          'Middle Eastern',
+          'Other'
+        ]}
+        value={race}
+        onChange={setRace}
+        clearable
       />
     </div>
   )

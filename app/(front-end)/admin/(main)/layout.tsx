cbfa98171828c456f8f0ea0cb11ea058c4615ce2
long <137044122+longleDevops@users.noreply.github.com>
2024-05-24 23:56:36 -0700

    disable eslint rules

--- app/(front-end)/admin/(main)/layout.tsx
+++ app/(front-end)/admin/(main)/layout.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "@/components/admin/sidebar"
 import styles from "./styles.module.css"
 import { useSettings } from "@/hooks/use-settings"
 const layout = ({ children }: { children: React.ReactNode }) => {
-  const { theme, setTheme } = useSettings()
+  const { theme } = useSettings()
   return (
     <div
       style={theme === 'Classic' ? { backgroundColor: 'white' } : {}}
